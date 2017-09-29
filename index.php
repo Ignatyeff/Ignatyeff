1bdd2f1b60dafab745bb2d9510413315bfa21943
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 17:26:11 +0300

    s

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ $app->initLayout('Centered');
 $button = $app->add('Button');
 $button->set('Steam');
 $button->icon = 'shopping bag';
+$button->set(['primary' => true]);
 $button->link('https://store.steampowered.com/');
