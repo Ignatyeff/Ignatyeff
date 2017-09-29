3a15330dfc9d8d5dedf0c9aba0911b51ef4bb267
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 17:16:00 +0300

    a

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add('Steam');
-$button->set('Кнопка');
+$button = $app->add('Button');
+$button->set('Steam');
 $button->icon = 'book';
 $button->link('https://store.steampowered.com/');
