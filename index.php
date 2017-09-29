6ca582916b1c0d8f0815002119dc5b25447dd389
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 16:52:30 +0300

    Page

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?PHP
+<?php
 require 'vendor/autoload.php';
-$app=new \atk4\ui\App('Просто сайт');
+$app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
+$button = $app->add(['Button' , 'Name']);
+$button->icon = 'book';
