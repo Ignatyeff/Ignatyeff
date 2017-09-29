529e3b30a17aeb12fbd1216b7722be10a433e891
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 17:21:59 +0300

    a

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
 $button = $app->add('Button');
 $button->set('Steam');
-$button->icon = 'book';
+$button->icon = 'shopping bag';
 $button->link('https://store.steampowered.com/');
