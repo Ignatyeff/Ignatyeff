69a9e481cd8bbd74146e2fb835cd3bf806a93d3a
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-06 15:17:06 +0300

    f

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add('Button');
+/*$button = $app->add('Button');
 $button->set('Steam');
 $button->icon = 'shopping bag';
 $button->set(['primary' => true]);
 $button->link('https://store.steampowered.com/');
-$button->color('black');
+$button->color('black');*/
+Class Car {
+  public $model;
+  public $speed;
+  public $color = 'white' ;
+  constant WHEELS = 4;
+  public function DisMeter($time) {
+    $distance= $this->speed * time;
+    return distance;
+  }
+  public function WheelCount() {
+    return self::WHEELS ;
+  }
+}
