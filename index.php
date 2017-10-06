31fbe11fd5c1f52e515fefef838a9fdf2fe8a50f
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-06 15:53:37 +0300

    в

--- index.php
+++ index.php
@@ -2,22 +2,60 @@
 require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
+$Label=$app->add('Label');
+$Label->set('Что-то');
+$Text=$app->add('Text');
+$Text->set('То-то');
+$Message=$app->add('Message');
+$Message->set('Дарова');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*$button = $app->add('Button');
 $button->set('Steam');
 $button->icon = 'shopping bag';
 $button->set(['primary' => true]);
 $button->link('https://store.steampowered.com/');
-$button->color('black');*/
-Class Car {
-  public $model;
-  public $speed;
-  public $color = 'white' ;
-  constant WHEELS = 4;
-  public function DisMeter($time) {
-    $distance= $this->speed * time;
-    return distance;
+Class Cat {
+  public $age;
+  public $gender;
+  public $name;
+  public $color = 'color' ;
+  const legs = 4;
+  public function DateOfBird($year) {
+    $age1= $year - $this->age;
+    return $age1;
   }
   public function WheelCount() {
     return self::WHEELS ;
   }
-}
+}*/
