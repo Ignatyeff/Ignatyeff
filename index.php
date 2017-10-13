c32d8ccfb6e9f0628dfd6810b10256f8ce8d7a04
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 16:36:37 +0300

    f

--- index.php
+++ index.php
@@ -2,68 +2,9 @@
 require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$Label=$app->add('Label');
-$Label->set('Что-то');
-$Text=$app->add('Text');
-$Text->set('То-то');
-$Message=$app->add('Message');
-$Message->set('Дарова');
-$button = $app->add('Button');
-$button->set('Steam');
-$button->icon = 'shopping bag';
-$button->set(['primary' => true]);
-$img = 'https://www.audi.co.uk/content/dam/audi/production/RestOfSite/ExploreModels/Finance/1x1_finance_calculator.jpg';
-$button = $app->add(['Label', 'hello', 'image'=>$img]);
-$button->set(['massive'=>true]);
-$icon = $app->add(['Image', $img, 'disabled']);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*$button = $app->add('Button');
-$button->set('Steam');
-$button->icon = 'shopping bag';
-$button->set(['primary' => true]);
-$button->link('https://store.steampowered.com/');
-Class Cat {
-  public $age;
-  public $gender;
-  public $name;
-  public $color = 'color' ;
-  const legs = 4;
-  public function DateOfBird($year) {
-    $age1= $year - $this->age;
-    return $age1;
-  }
-  public function WheelCount() {
-    return self::WHEELS ;
-  }
-}*/
+$button = $app->layout->add(['Button', 'HELLO']);
+$button->addClass('mini');
+$button2 = $app->layout->add(['Button', 'BYE']);
+$button2->addClass('black');
+$button3 = $app->layout->add(['Button', 'WHATEWER']);
+$button3->addClass('big orange');
