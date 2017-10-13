ee1d5a5d9746e51487d1a713638ea3034d8f1ac4
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 16:52:31 +0300

    g

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $button = $app->layout->add(['Button', 'HELLO']);
 $button->addClass('mini');
 $button2 = $app->layout->add(['Button', 'BYE']);
 $button2->addClass('black');
-$button2->link(['index']);
+$button2->link(['index(1)']);
 $button3 = $app->layout->add(['Button', 'WHATEWER']);
 $button3->addClass('big orange');
 $button3 = $app->layout->add(['Button', 'LINK TO TXT FILE']);
