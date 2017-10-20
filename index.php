ada10c0615e88d84d29bc871c09888e81bbaf901
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-20 18:02:49 +0300

    d

--- index.php
+++ index.php
@@ -3,10 +3,17 @@ require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Game');
 $app->initLayout('Centered');
 if(isset($_GET['max'])) {
+
   $max = $_GET['max'];
   $min = $_GET['min'];
   $number=round(($max+$min)/2);
   $label = $app->add(['Label','Твоё число больше '.$number.' ?']);
+  if(isset($_GET['greater'])) {
+    $min=$number;
+  }
+  if(isset($_GET['less'])) {
+    $max=$number;
+  }
 } else {
   $max=1;
   $min=100;
@@ -19,6 +26,8 @@ if(isset($_GET['max'])) {
 $button = $app->add(['Button','Больше!']);
 $button->link(['index', 'max'=>$max, 'min'=>$min, 'greater'=>TRUE]);
 
-$button2 = $app->add(['Button','Да, это мое число!'])
+$button2 = $app->add(['Button','Да, это мое число!']);
+
+
 $button3 = $app->add(['Button','Меньше!']);
 $button->link(['index', 'max'=>$max, 'min'=>$min, 'less'=>TRUE]);
