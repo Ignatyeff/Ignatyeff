89cb21dbcc4985144691829521e8519a89ac13cc
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-20 17:50:42 +0300

    f

--- index.php
+++ index.php
@@ -1,81 +1,24 @@
 <?php
 require 'vendor/autoload.php';
-$app=new \atk4\ui\App('Registration');
+$app=new \atk4\ui\App('Game');
 $app->initLayout('Centered');
-$button = $app->layout->add(['Button', '1']);
-$button->link(['index(1)', 'number'=>'1', 'button'=>'red']);
-
-
-$button2 = $app->layout->add(['Button', '2']);
-$button2->link(['index(1)', 'number'=>'2', 'button'=>'red']);
-
-
-$button3 = $app->layout->add(['Button', '3']);
-$button3->link(['index(1)', 'number'=>'3', 'button'=>'red']);
-
-
-
+if(isset($_GET['max'])) {
+  $max = $_GET['max'];
+  $min = $_GET['min'];
+  $number=round(($max+$min)/2);
+  $label = $app->add(['Label','Твоё число больше '.$number.' ?']);
+} else {
+  $max=1;
+  $min=100;
+  $number=round(($max+$min)/2);
+}
 
 
 
 
+$button = $app->add(['Button','Больше!']);
+$button->link(['index', 'max'=>$max, 'min'=>$min, 'greater'=>TRUE]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*$button = $app->layout->add(['Button', 'HELLO']);
-$button->addClass('mini');
-$button2 = $app->layout->add(['Button', 'BYE']);
-$button2->addClass('black');
-$button2->link(['index(1)', 'language'=>'abc', 'Country'=>'Russia']);
-$button3 = $app->layout->add(['Button', 'WHATEVER']);
-$button3->addClass('big orange');
-$button3 = $app->layout->add(['Button', 'LINK TO TXT FILE']);
-$button3->link('my_document.txt');/*
+$button2 = $app->add(['Button','Да, это мое число!'])
+$button3 = $app->add(['Button','Меньше!']);
+$button->link(['index', 'max'=>$max, 'min'=>$min, 'less'=>TRUE]);
