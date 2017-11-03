e005888bba1b94e7e80ee0454efb42f569deca37
Ignatyeff <boroda42017@gmail.com>
2017-11-03 17:13:31 +0200

    f

--- index(lul).php
+++ index(lul).php
@@ -2,7 +2,11 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('index');
 $app->initLayout('Centered');
-if(isset($_GET['max'])){
+if(isset($_GET['Нет'])){
+if($min==$max){
+$label=$app->add(['Label','УНТЕРМЕНШ']);
+$label->addClass('massive red');
+}
   $max = $_GET['max'];
   $min = $_GET['min'];
   $med=round(($max+$min)/2);
@@ -11,15 +15,16 @@ if(isset($_GET['max'])){
   $min=1;
   $med=round(($max+$min)/2);
 }
-$label=$app ->add(['Label','TVOE 4ISLO BOLSHE '.$med.' ?']);
 
-$button1=$app->add(['Button','DA']);
-$button1->link(['index(lul)','max'=>$max,'min'=>$med]);
+$label=$app ->add(['Label','Твоё число больше'.$med.' ?']);
 
-$button2=$app->add(['Button','NEJ']);
-$button2->link(['index(lul)','max'=>$med,'min'=>$min]);
+$button1=$app->add(['Button','ДА']);
+$button1->link(['index(lul)','max'=>$max,'min'=>$med,'Да'=>'null']);
 
-$button3=$app->add(['Button','O DA']);
+$button2=$app->add(['Button','НЕТ']);
+$button2->link(['index(lul)','max'=>$med,'min'=>$min, 'Нет'=>'null']);
+
+$button3=$app->add(['Button','О ДА']);
 $button3->link(['pustoj','med'=>$med]);
 
 
