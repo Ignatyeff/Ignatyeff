1dfa805c5d1b5e4aa0ef10bd794038172b8226c7
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-11-24 17:26:42 +0200

    d
    d

--- index.php
+++ index.php
@@ -3,15 +3,26 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('index');
 $app->initLayout('Centered');
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=Accounts;host=localhost','root','');
+\atk4\data\Persistence_SQL('mysql:dbname=accounts;host=localhost','root','');
 class User extends \atk4\data\Model {
   public $table = 'Dates';
 function init() {
 	parent::init();
-	$this->addField('email');
-	$this->addField('login(username)');
-	$this->addField('password');
-  $this->addField('creation_date',['type'=>'date']);
-  $this->addField('account_money',['type'=>'money']);
+	$this->addField('name');
+	$this->addField('surname');
+	$this->addField('phone_number',['default'=>'+371']);
+  $this->addField('email');
+  $this->addField('password',['type'=>'password']);
+  $this->addField('birthday',['type'=>'date']);
+  $this->addField('notes',['type'=>'text']);
 }
 }
+
+$form = $app->layout->add('Form'); // создание формы
+$form->setModel(new User($db)); // подключение формы к модели и соединение с базой данных
+
+$form->onSubmit(function($form) { // функция срабатывающая при нажатии кнопки SAVE
+	$form->model->save(); // запись всех данных в нужную таблицу
+	return $form->success('Record updated'); // информация, что всё удачно записалось
+
+});
